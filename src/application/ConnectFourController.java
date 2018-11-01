5e23fdb612002d180f07d5f53ce936a8c7e80fbf
row49382 <r-wroblewski@bethel.edu>
2018-11-01 01:19:34 -0500

    decoupled the view and controller from the main class. The view is now in an fxml file and controller in it's own java file. Main will only load the fxml file and show the scene.

---
+++ src/application/ConnectFourController.java
@@ -0,0 +1,240 @@
+package application;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import application.models.ConnectFour;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+public class ConnectFourController implements EventHandler<ActionEvent>, Observer
+{
+	private final static String RED_TURN_TEXT = "It is RED Player's turn. Click a button to make a move.";
+	private final static String BLACK_TURN_TEXT = "It is BLACK Player's turn. Click a button to make a move.";
+		
+	/** The graphic game board of the gui */
+	@FXML
+	private CircleButton[][] guiBoard;
+	
+	/** The gridpane that holds the gui board */
+	@FXML
+	private GridPane guiBoardGP;
+	
+	/** Resets the board */
+	@FXML
+	private Button resetButton;
+	
+	/** Undos the last turn */
+	@FXML
+	private Button undoButton;
+	
+	/** Holds the buttons */
+	@FXML
+	private HBox buttonBox;
+	
+	/** Holds the value of the textfield that displays the game status */
+	@FXML
+	private Label banner;
+	
+	/** Holds the pointer to the game board */
+	private ConnectFour game;
+
+	/** Holds the integer value of the column last moved that is passed into the game */
+	private int currentColumn;
+	
+	@FXML
+	private void initialize()
+	{
+		this.game = new ConnectFour("R","B","-");
+		this.game.addObserver(this);
+		
+		this.fillBoard();
+		this.banner.setText(RED_TURN_TEXT);
+		this.currentColumn = -1;
+	}
+	
+	/**
+	 * Fills the board for the gui
+	 * @param mp The Mainpane that is added to the gui
+	 */
+	public void fillBoard()
+	{	
+		this.guiBoard = new CircleButton[6][7];
+		
+		for (int i = 0; i < this.game.getGameBoard().length; i++)
+		{	
+			for (int j = 0; j < this.game.getGameBoard()[i].length; j++)
+			{
+				CircleButton button = new CircleButton(i, j);
+				button.setOnAction((x) -> 
+				{
+					this.currentColumn = button.getColumn();
+					
+					try 
+					{
+						this.game.makeMove(currentColumn);	
+					}
+					catch (Exception e) 
+					{
+						Alert errorAlert = new Alert(AlertType.ERROR, e.getMessage());
+						errorAlert.showAndWait();
+					}
+					
+					this.updateBoardText();
+							
+					// check if the game is over after every move
+					this.checkGameOver();
+				});
+			
+				this.guiBoard[i][j] = button;
+				this.guiBoardGP.add(this.guiBoard[i][j], j, i);
+			}		
+		}	
+		//mp.add(guiBoardGP, 1, 2);
+	}	
+	
+	/**
+	 * Updates the gameboard after the observer is notified
+	 */
+	private void updateBoard()
+	{
+		if (this.game.isFirstPlayer())
+		{
+			this.guiBoard[this.game.getColumnSpaces()[this.currentColumn] + 1][this.currentColumn].setColor("Black");
+		}
+		else
+		{
+			this.guiBoard[this.game.getColumnSpaces()[this.currentColumn] + 1][this.currentColumn].setColor("Red");
+		}
+	}
+	
+	/**
+	 * Sets the board text every move based on if it's the first
+	 * players turn 
+	 */
+	private void updateBoardText()
+	{
+		if (!this.game.isFirstPlayer()) 
+		{	
+			this.banner.setText(BLACK_TURN_TEXT);
+		}	
+		else
+		{	
+			this.banner.setText(RED_TURN_TEXT);
+		}
+	}
+	
+
+	/**
+	 * disables all board buttons in the  when the game is over
+	 */
+	private void lockBoard()
+	{
+		for (int i = 0; i < this.guiBoard.length; i++)
+		{
+			for (int j = 0; j < this.guiBoard[i].length; j++)
+			{
+				this.guiBoard[i][j].setDisable(true);
+			}
+		}
+		this.undoButton.setDisable(true);
+	}
+	
+	/**
+	 * Resets the board to make all buttons yellow and clickable
+	 */
+	private void resetBoard()
+	{
+		for (int i = 0; i < this.guiBoard.length; i++)
+		{
+			for (int j = 0; j < this.guiBoard[i].length; j++)
+			{
+				this.guiBoard[i][j].setDefaultStyle();
+				this.guiBoard[i][j].setDisable(false);
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the game is over after making a move
+	 */
+	private void checkGameOver()
+	{
+		if (this.game.gameOver())
+		{
+			StringBuilder endOfGameText = new StringBuilder(20);
+			
+			if (this.game.isTie())
+			{
+				endOfGameText.append("The game is a tie. ");
+			}
+			else if (this.game.isFirstPlayer())
+			{
+				endOfGameText.append("The winner is BLACK! ");
+			}
+			else
+			{
+				endOfGameText.append("The winner is RED! ");
+			}
+			
+			endOfGameText.append("Click restart to play again..");
+			this.banner.setText(endOfGameText.toString());
+			
+			this.lockBoard();
+		}
+	}
+	
+	@Override
+	public void handle(ActionEvent event) 
+	{	
+		if (event.getSource() == this.resetButton)
+		{
+			this.game.reset();
+		}		
+		else if (event.getSource() == this.undoButton)
+		{
+			this.game.undoMove();
+		}
+	}
+
+	@Override
+	public void update(Observable o, Object arg) 
+	{
+		if (arg != null)
+		{
+			if ((arg.toString()).equals("MakeMove"))
+			{
+				this.currentColumn = this.game.getPreviousColumn().peek();	
+				this.undoButton.setDisable(false);
+				this.updateBoard();	
+			}
+			else if (arg instanceof Integer)
+			{
+				// passes the column value of the recently made move
+				this.currentColumn = (int)arg;
+				this.guiBoard[this.game.getColumnSpaces()[this.currentColumn]][this.currentColumn].setDefaultStyle();
+				
+				if (this.game.getPreviousColumn().isEmpty())
+				{
+					this.undoButton.setDisable(true);
+				}
+				
+				this.updateBoardText();
+			}
+		}
+		else
+		{
+			// reset was clicked and the board needs to be reset
+			this.resetBoard();
+			currentColumn = -1;
+			this.banner.setText(RED_TURN_TEXT);
+		}
+	}
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,8 @@ import application.models.ConnectFour;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -23,45 +25,14 @@ import javafx.scene.shape.Circle;
  * @author Robert Wroblewski
  *
  */
-public class Main extends Application implements EventHandler<ActionEvent>, Observer
+public class Main extends Application
 {
-	
-	private final static String RED_TURN_TEXT = "It is RED Player's turn. Click a button to make a move.";
-	private final static String BLACK_TURN_TEXT = "It is BLACK Player's turn. Click a button to make a move.";
-	
-	/** Holds the pointer to the game board */
-	private ConnectFour game;
-	
-	/** The graphic game board of the gui */
-	private CircleButton[][] guiBoard;
-	
-	/** Resets the board */
-	private Button resetButton;
-	
-	/** Undos the last turn */
-	private Button undoButton;
-	
-	/** Holds the buttons */
-	private HBox buttonBox;
-	
-	/** Holds the value of the textfield that displays the game status */
-	private Label banner;
-	
-	/** Holds the integer value of the column last moved that is passed into the game */
-	private int currentColumn;
-	
 	@Override
 	public void start(Stage primaryStage) 
 	{
-		this.game = new ConnectFour("R","B","-");
-		this.game.addObserver(this);
-		
-		this.guiBoard = new CircleButton[6][7];
-		this.currentColumn = -1;
-		
 		try 
 		{
-			GridPane root = createMainPane();
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/application/views/ConnectFourView.fxml"));
 			Scene scene = new Scene(root, 500, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Connect Four");
@@ -70,212 +41,7 @@ public class Main extends Application implements EventHandler<ActionEvent>, Obse
 		} 
 		catch(Exception e) 
 		{
-			e.getStackTrace();
-		}
-	}
-	
-	/**
-	 * Creates the main pane
-	 * @return The main pane 
-	 */
-	public GridPane createMainPane()
-	{
-		GridPane mp = new GridPane();
-		mp.setVgap(20);
-		mp.setHgap(20);
-		
-		this.fillBoard(mp);
-		
-		this.resetButton = new Button("Restart");
-		this.resetButton.setOnAction(this);
-		
-		this.undoButton = new Button("Undo");
-		this.undoButton.setDisable(true);
-		this.undoButton.setOnAction(this);
-		
-		this.buttonBox = new HBox(10, this.resetButton, this.undoButton);
-		mp.add(buttonBox, 1, 3);
-				
-		this.banner = new Label(RED_TURN_TEXT);
-		mp.add(this.banner, 1, 1);
-		
-		return mp;
-	}
-	
-	/**
-	 * Fills the board for the gui
-	 * @param mp The Mainpane that is added to the gui
-	 */
-	public void fillBoard(GridPane mp)
-	{	
-		GridPane boardPane = new GridPane();
-		for (int i = 0; i < this.game.getGameBoard().length; i++)
-		{	
-			for (int j = 0; j < this.game.getGameBoard()[i].length; j++)
-			{
-				CircleButton button = new CircleButton(i, j);
-				button.setOnAction((x) -> 
-				{
-					this.currentColumn = button.getColumn();
-					
-					try 
-					{
-						this.game.makeMove(currentColumn);	
-					}
-					catch (Exception e) 
-					{
-						Alert errorAlert = new Alert(AlertType.ERROR, e.getMessage());
-						errorAlert.showAndWait();
-					}
-					
-					this.updateBoardText();
-							
-					// check if the game is over after every move
-					this.checkGameOver();
-				});
-			
-				this.guiBoard[i][j] = button;
-				boardPane.add(this.guiBoard[i][j], j, i);
-			}		
-		}	
-		mp.add(boardPane, 1, 2);
-	}	
-	
-	/**
-	 * Updates the gameboard after the observer is notified
-	 */
-	private void updateBoard()
-	{
-		if (this.game.isFirstPlayer())
-		{
-			this.guiBoard[this.game.getColumnSpaces()[this.currentColumn] + 1][this.currentColumn].setColor("Black");
-		}
-		else
-		{
-			this.guiBoard[this.game.getColumnSpaces()[this.currentColumn] + 1][this.currentColumn].setColor("Red");
-		}
-	}
-	
-	/**
-	 * Sets the board text every move based on if it's the first
-	 * players turn 
-	 */
-	private void updateBoardText()
-	{
-		if (!this.game.isFirstPlayer()) 
-		{	
-			this.banner.setText(BLACK_TURN_TEXT);
-		}	
-		else
-		{	
-			this.banner.setText(RED_TURN_TEXT);
-		}
-	}
-	
-
-	/**
-	 * disables all board buttons in the  when the game is over
-	 */
-	private void lockBoard()
-	{
-		for (int i = 0; i < this.guiBoard.length; i++)
-		{
-			for (int j = 0; j < this.guiBoard[i].length; j++)
-			{
-				this.guiBoard[i][j].setDisable(true);
-			}
-		}
-		this.undoButton.setDisable(true);
-	}
-	
-	/**
-	 * Resets the board to make all buttons yellow and clickable
-	 */
-	private void resetBoard()
-	{
-		for (int i = 0; i < this.guiBoard.length; i++)
-		{
-			for (int j = 0; j < this.guiBoard[i].length; j++)
-			{
-				this.guiBoard[i][j].setDefaultStyle();
-				this.guiBoard[i][j].setDisable(false);
-			}
-		}
-	}
-	
-	/**
-	 * Checks if the game is over after making a move
-	 */
-	private void checkGameOver()
-	{
-		if (this.game.gameOver())
-		{
-			StringBuilder endOfGameText = new StringBuilder(20);
-			
-			if (this.game.isTie())
-			{
-				endOfGameText.append("The game is a tie. ");
-			}
-			else if (this.game.isFirstPlayer())
-			{
-				endOfGameText.append("The winner is BLACK! ");
-			}
-			else
-			{
-				endOfGameText.append("The winner is RED! ");
-			}
-			
-			endOfGameText.append("Click restart to play again..");
-			this.banner.setText(endOfGameText.toString());
-			
-			this.lockBoard();
-		}
-	}
-	
-	@Override
-	public void handle(ActionEvent event) 
-	{	
-		if (event.getSource() == this.resetButton)
-		{
-			this.game.reset();
-		}		
-		else if (event.getSource() == this.undoButton)
-		{
-			this.game.undoMove();
-		}
-	}
-
-	@Override
-	public void update(Observable o, Object arg) 
-	{
-		if (arg != null)
-		{
-			if ((arg.toString()).equals("MakeMove"))
-			{
-				// passes the column value of the recently made move
-				this.currentColumn = this.game.getPreviousColumn().peek();	
-				this.undoButton.setDisable(false);
-				this.updateBoard();	
-			}
-			else if (arg instanceof Integer)
-			{
-				this.currentColumn = (int)arg;
-				this.guiBoard[this.game.getColumnSpaces()[this.currentColumn]][this.currentColumn].setDefaultStyle();
-				
-				if (this.game.getPreviousColumn().isEmpty())
-				{
-					this.undoButton.setDisable(true);
-				}
-				
-				this.updateBoardText();
-			}
-		}
-		else
-		{
-			// reset was clicked and the board needs to be reset
-			this.resetBoard();
-			currentColumn = -1;
-			this.banner.setText(RED_TURN_TEXT);
+			System.out.println(e.getMessage() + "\n" + e.getCause());
 		}
 	}
 	
